307c63311a175ee2d30f389a86fa58dc2261eb73
Sajad <sajadrahimi0101@gmail.com>
2023-06-27 14:34:33 +0330

    creatd screens for show nurses and nurse details

--- lib/views/pages/reserve_nurse/show_nerse_screen.dart
+++ lib/views/pages/reserve_nurse/show_nerse_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:rayaniyaresh/views/pages/reserve_nurse/nurse_detail_screen.dart';
+import 'package:rayaniyaresh/views/widgets/nurse_widget.dart';
 
 class ShowNerseScreen extends StatelessWidget {
   const ShowNerseScreen({Key? key}) : super(key: key);
@@ -7,128 +9,28 @@ class ShowNerseScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color(0xffF8FAFB),
-        title: const Text(
-          "پرستار ها",
-          style: TextStyle(color: Color(0xff000000)),
+        appBar: AppBar(
+          backgroundColor: const Color(0xffF8FAFB),
+          title: const Text(
+            "پرستار ها",
+            style: TextStyle(color: Color(0xff000000)),
+          ),
+          centerTitle: true,
+          leading: const Icon(Icons.more_vert, color: Color(0xff000000)),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.arrow_forward, color: Color(0xff000000)),
+              onPressed: () => Get.back(),
+            )
+          ],
         ),
-        centerTitle: true,
-        leading: const Icon(Icons.more_vert, color: Color(0xff000000)),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.arrow_forward, color: Color(0xff000000)),
-            onPressed: () => Get.back(),
-          )
-        ],
-      ),
-      body: ListView.builder(
-          physics: BouncingScrollPhysics(),
-          itemCount: 10,
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          itemBuilder: (_, index) => Card(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)),
-                child: Container(
-                  margin: const EdgeInsets.symmetric(vertical: 10),
-                  width: Get.width,
-                  height: Get.height / 5.6,
-                  child: Row(
-                    children: [
-                      // image
-                      Padding(
-                        padding: const EdgeInsets.only(right: 5),
-                        child: Container(
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              image: const DecorationImage(
-                                  image: NetworkImage(
-                                      "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200"))),
-                          child: SizedBox.square(
-                            dimension: Get.width / 3.2,
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: Get.height,
-                        width: Get.width / 18,
-                      ),
-                      // text an other
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 8),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              // name
-                              Text(
-                                "آقای سجاد رحیمی",
-                                style: TextStyle(
-                                  color: Color(0xff000000),
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: Get.width / 25,
-                                ),
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                              // expertise
-                              Text(
-                                "پرستار",
-                                style: TextStyle(
-                                  color: Color(0xffa0a0a0),
-                                  fontSize: Get.width / 28,
-                                ),
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                              // star rate
-                              Container(
-                                padding:
-                                    const EdgeInsets.symmetric(vertical: 1),
-                                width: Get.width / 6.8,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(5),
-                                    color: const Color(0xff62E067)),
-                                child: Row(children: [
-                                  const Icon(
-                                    Icons.star,
-                                    color: Colors.green,
-                                  ),
-                                  Text(
-                                    " 4.5",
-                                    style: TextStyle(
-                                      color: const Color(0xff000000),
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: Get.width / 28,
-                                    ),
-                                    overflow: TextOverflow.ellipsis,
-                                  )
-                                ]),
-                              ),
-                              // location text
-                              Row(
-                                children: [
-                                  const Icon(
-                                    Icons.location_on,
-                                    color: Color(0xffa0a0a0),
-                                  ),
-                                  Text(
-                                    " البرز / کرج",
-                                    style: TextStyle(
-                                      color: const Color(0xffa0a0a0),
-                                      fontSize: Get.width / 28,
-                                    ),
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              )),
-    );
+        body: ListView.builder(
+            physics: const BouncingScrollPhysics(),
+            itemCount: 10,
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            itemBuilder: (_, index) => NurseWidget(
+                  onTap: () => Get.to(() => const NurseDetailScreen(),
+                      transition: Transition.rightToLeft),
+                )));
   }
 }
---
+++ lib/views/widgets/nurse_widget.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+class NurseWidget extends StatelessWidget {
+  const NurseWidget({Key? key, this.onTap,this.evalation}) : super(key: key);
+  final void Function()? onTap;
+  final double? evalation;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Card(
+        elevation: evalation,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        child: Container(
+          margin: const EdgeInsets.symmetric(vertical: 10),
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height / 5.6,
+          child: Row(
+            children: [
+              // image
+              Padding(
+                padding: const EdgeInsets.only(right: 5),
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: const DecorationImage(
+                          image: NetworkImage(
+                              "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200"))),
+                  child: SizedBox.square(
+                    dimension: MediaQuery.of(context).size.width / 3.2,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width / 18,
+              ),
+              // text an other
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      // name
+                      Text(
+                        "آقای سجاد رحیمی",
+                        style: TextStyle(
+                          color: Color(0xff000000),
+                          fontWeight: FontWeight.bold,
+                          fontSize: MediaQuery.of(context).size.width / 25,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      // expertise
+                      Text(
+                        "پرستار",
+                        style: TextStyle(
+                          color: Color(0xffa0a0a0),
+                          fontSize: MediaQuery.of(context).size.width / 28,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      // star rate
+                      Container(
+                        padding: const EdgeInsets.symmetric(vertical: 1),
+                        width: MediaQuery.of(context).size.width / 6.8,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            color: const Color(0xff62E067)),
+                        child: Row(children: [
+                          const Icon(
+                            Icons.star,
+                            color: Colors.green,
+                          ),
+                          Text(
+                            " 4.5",
+                            style: TextStyle(
+                              color: const Color(0xff000000),
+                              fontWeight: FontWeight.bold,
+                              fontSize: MediaQuery.of(context).size.width / 28,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          )
+                        ]),
+                      ),
+                      // location text
+                      Row(
+                        children: [
+                          const Icon(
+                            Icons.location_on,
+                            color: Color(0xffa0a0a0),
+                          ),
+                          Text(
+                            " البرز / کرج",
+                            style: TextStyle(
+                              color: const Color(0xffa0a0a0),
+                              fontSize: MediaQuery.of(context).size.width / 28,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
