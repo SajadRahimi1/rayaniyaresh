1a5b6ddbc89413b28c68251694f1bd9422ab87df
Sajad <sajadrahimi0101@gmail.com>
2023-07-17 14:14:23 +0330

    increase time out of get connect

--- lib/models/constants/get_connect.dart
+++ lib/models/constants/get_connect.dart
@@ -1,3 +1,4 @@
 import 'package:get/get_connect/connect.dart';
 
-final GetConnect getConnect = GetConnect(allowAutoSignedCert: true);
+final GetConnect getConnect =
+    GetConnect(allowAutoSignedCert: true, timeout: const Duration(seconds: 8));
