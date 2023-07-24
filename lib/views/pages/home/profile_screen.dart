14132f14b7aa08bdc9627ff96e7a3141586dc5b7
Sajad <sajadrahimi0101@gmail.com>
2023-07-24 17:20:47 +0330

    disable profile connected to api

--- lib/views/pages/home/profile_screen.dart
+++ lib/views/pages/home/profile_screen.dart
@@ -128,8 +128,9 @@ class ProfileScreen extends StatelessWidget {
                           backgroundImage:
                               SingletonClass().imageUrl?.isEmpty ?? true
                                   ? null
-                                  : CachedNetworkImageProvider(baseUrl +
-                                      '/uploads/' +
+                                  : CachedNetworkImageProvider(
+                                      // baseUrl +
+                                      //   '/uploads/' +
                                       (SingletonClass().imageUrl ?? ""))
                           //  const NetworkImage(
                           //     "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200"),
