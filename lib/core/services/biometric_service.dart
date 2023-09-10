56725b36319460ffa9d3ee0458db4240919fd23d
Sajad <sajadrahimi0101@gmail.com>
2023-09-10 13:25:02 +0330

    customize title of fingerpirnt dielog

--- lib/core/services/biometric_service.dart
+++ lib/core/services/biometric_service.dart
@@ -1,11 +1,11 @@
 import 'package:local_auth/local_auth.dart';
+import 'package:local_auth_android/local_auth_android.dart';
 
 class BiometricService {
   bool canAuthenticateWithBiometrics = false;
   bool canAuthenticate = false;
   final LocalAuthentication auth = LocalAuthentication();
   List<BiometricType> availableBiometrics = [];
-  
 
   Future<void> fillVariable() async {
     canAuthenticateWithBiometrics = await auth.canCheckBiometrics;
@@ -20,8 +20,10 @@ class BiometricService {
     try {
       if (canAuthenticateWithBiometrics && canAuthenticate) {
         return await auth.authenticate(
-          localizedReason: 'برای ورود اثرانگشت خود را تایید کنید',
-        );
+            localizedReason: 'برای ورود اثرانگشت خود را تایید کنید',
+            authMessages: const <AuthMessages>[
+              AndroidAuthMessages(signInTitle: 'آسیاسلامت', biometricHint: "",),
+            ]);
       }
     } catch (_) {
       return false;
