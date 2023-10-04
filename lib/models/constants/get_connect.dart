da729353471f09e60ac2225cf3655df202fe6bdc
Sajad Rahimi <75030204+SajadRahimi1@users.noreply.github.com>
2023-10-04 19:53:10 +0330

    Update get_connect.dart

--- lib/models/constants/get_connect.dart
+++ lib/models/constants/get_connect.dart
@@ -1,4 +1,4 @@
 import 'package:get/get_connect/connect.dart';
 
 final GetConnect getConnect =
-    GetConnect(allowAutoSignedCert: true, timeout: const Duration(seconds: 12));
+    GetConnect(allowAutoSignedCert: true, timeout: const Duration(seconds: 20));
